7e14813e48162feab915e5bee3ae8594970b2dc1
Suhag Al Amin <developersuhag@gmail.com>
2021-11-10 00:56:04 +0600

    connect to database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,6 +10,25 @@ const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+// connection uri
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qow90.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected");
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Carx Server is running");
