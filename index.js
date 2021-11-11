6fd8ea98329af87ad73034418d50d83e508c4977
Suhag Al Amin <developersuhag@gmail.com>
2021-11-11 22:22:02 +0600

    fix load orders

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
     // get order by user emails
     app.get("/orders", async (req, res) => {
       const email = req.query.email;
-      const query = { email: email };
+      const query = { userEmail: email };
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
       res.json(orders);
