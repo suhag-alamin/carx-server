13c0fefd8443dee3a0af39a0cc6c9b228a811999
suhag-alamin <developersuhag@gmail.com>
2023-11-27 16:05:06 +0600

    Removed dist from Git

--- dist/app/modules/user/user.constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userFilterableFields = exports.userSearchableFields = exports.userRoles = void 0;
-exports.userRoles = ['user', 'admin', 'super-admin'];
-exports.userSearchableFields = ['displayName', 'email'];
-exports.userFilterableFields = ['query', 'displayName', 'email', 'role'];
